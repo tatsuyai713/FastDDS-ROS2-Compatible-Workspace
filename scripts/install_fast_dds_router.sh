9740c22526562c5a69c317bdd06940d5de5a6f1f
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2022-11-01 01:47:28 +0900

    add Fast DDS Router install script

---
+++ scripts/install_fast_dds_router.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+sudo pip3 install -U vcstool pyyaml jsonschema
+sudo apt install -y libasio-dev libtinyxml2-dev libssl-dev libyaml-cpp-dev
+
+cd ~
+sudo rm -rf DDS-Router-1.0.0*
+wget https://github.com/eProsima/DDS-Router/archive/refs/tags/v1.0.0.tar.gz -O DDS-Router-1.0.0.tar.gz
+
+tar xvf ./DDS-Router-1.0.0.tar.gz
+
+mkdir -p ~/DDS-Router-1.0.0/src
+mkdir -p ~/DDS-Router-1.0.0/build
+cd ~/DDS-Router-1.0.0
+wget https://raw.githubusercontent.com/eProsima/DDS-Router/main/ddsrouter.repos
+vcs import src < ddsrouter.repos
+
+# CMake Utils
+cd ~/DDS-Router-1.0.0
+mkdir build/cmake_utils
+cd build/cmake_utils
+cmake ~/DDS-Router-1.0.0/src/dev-utils/cmake_utils -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCMAKE_PREFIX_PATH=/usr/local/
+sudo cmake --build . --target install
+
+# C++ Utils
+cd ~/DDS-Router-1.0.0
+mkdir build/cpp_utils
+cd build/cpp_utils
+cmake ~/DDS-Router-1.0.0/src/dev-utils/cpp_utils -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCMAKE_PREFIX_PATH=/usr/local/
+sudo cmake --build . --target install
+
+# ddsrouter_core
+cd ~/DDS-Router-1.0.0
+mkdir build/ddsrouter_core
+cd build/ddsrouter_core
+cmake ~/DDS-Router-1.0.0/src/ddsrouter/ddsrouter_core -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCMAKE_PREFIX_PATH=/usr/local/
+sudo cmake --build . --target install
+
+# ddsrouter_yaml
+cd ~/DDS-Router-1.0.0
+mkdir build/ddsrouter_yaml
+cd build/ddsrouter_yaml
+cmake ~/DDS-Router-1.0.0/src/ddsrouter/ddsrouter_yaml -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCMAKE_PREFIX_PATH=/usr/local/
+sudo cmake --build . --target install
+
+# ddsrouter_tool
+cd ~/DDS-Router-1.0.0
+mkdir build/ddsrouter_tool
+cd build/ddsrouter_tool
+cmake ~/DDS-Router-1.0.0/src/ddsrouter/tools/ddsrouter_tool -DCMAKE_INSTALL_PREFIX=/usr/local/ -DCMAKE_PREFIX_PATH=/usr/local/
+sudo cmake --build . --target install
