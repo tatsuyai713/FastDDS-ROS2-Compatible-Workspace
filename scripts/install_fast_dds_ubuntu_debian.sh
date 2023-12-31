2bbd9ee16c812114a9a26e84029487e017561af9
Tatsuya Ishikawa <34103899+tatsuyai713@users.noreply.github.com>
2023-12-31 18:40:18 +0900

    Update install_fast_dds_ubuntu_debian.sh

--- scripts/install_fast_dds_ubuntu_debian.sh
+++ scripts/install_fast_dds_ubuntu_debian.sh
@@ -117,8 +117,7 @@ sed -i -e '/export PATH=$PATH:\/opt\/fast-dds-gen\/bin/d' ~/.bashrc
 echo 'export PATH=$PATH:/opt/fast-dds-gen/bin' >> ~/.bashrc
 
 
-grep 'export LD_LIBRARY_PATH=/opt/fast-dds/lib:$LD_LIBRARY_PATH' ~/.bashrc
-if [ $? = 0 ]; then
+if grep 'export LD_LIBRARY_PATH=/opt/fast-dds/lib:$LD_LIBRARY_PATH' ~/.bashrc >/dev/null; then
   echo "LD_LIBRARY_PATH libs are already added"
 else
   echo 'export LD_LIBRARY_PATH=/opt/fast-dds/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
