56f73b82ad5cdef51f93640c607dadfca30d170e
Tatsuya Ishikawa <34103899+tatsuyai713@users.noreply.github.com>
2023-12-31 16:45:19 +0900

    Update install_fast_dds_archlinux.sh

--- scripts/install_fast_dds_archlinux.sh
+++ scripts/install_fast_dds_archlinux.sh
@@ -110,7 +110,7 @@ sudo mkdir -p /opt/fast-dds-gen
 git clone --recursive -b v$fast_dds_gen_version https://github.com/eProsima/Fast-DDS-Gen.git fast-dds-gen \
     && cd fast-dds-gen \
     && gradle assemble \
-    && sudo -E gradle install --install_path=/opt/fast-dds-gen
+    && sudo /opt/gradle/gradle-7.5.1/bin/gradle install --install_path=/opt/fast-dds-gen
 
 sed -i -e '/export PATH=$PATH:\/opt\/fast-dds-gen\/scripts/d' ~/.bashrc
 echo 'export PATH=$PATH:/opt/fast-dds-gen/scripts' >> ~/.bashrc
